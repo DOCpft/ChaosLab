d488658826267229d16a0aebe91d7e9ff6f23bb0
Kirill <kirill.tkachenko02@bk.ru>
2026-04-08 17:19:02 +0300

    Изменил регистрацию модуля RabbitMQ с использованием golevelup/nestjs-rabbitmq. И отправку сообщения тоже с помощью него. Т.к. ClientProxy не хотел отправлять сообщение в exchange

--- packages/api-gateway/src/rabbitmq/rabbitmq.module.ts
+++ packages/api-gateway/src/rabbitmq/rabbitmq.module.ts
@@ -1,28 +1,35 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices'
 import { RabbitMQService } from './rabbitmq.service';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
     imports: [
-        ClientsModule.register([
-            {
-                name: 'RABBITMQ_CLIENT',
-                transport: Transport.RMQ,
-                options: {
-                    urls: [process.env.RABBITMQ_URL || 'amqp://chaos_user:chaos_password@localhost:5672'],
-                    queue: 'orchestrator_queue',
-                    queueOptions: {
-                        durable: true
-                    },
-                    noAck: true, // пока нет оркестратора
-                    persistent: true
-                }
-            }
-        ]),
+        // ClientsModule.register([
+        //     {
+        //         name: 'RABBITMQ_CLIENT',
+        //         transport: Transport.RMQ,
+        //         options: {
+        //             urls: [process.env.RABBITMQ_URL || 'amqp://chaos_user:chaos_password@localhost:5672'],
+        //             exchange: 'chaos.exchange',
+        //             exchangeType: 'topic',
+        //             routingKey: 'chaos.orchestrator',
+        //             noAck: true, // пока нет оркестратора
+        //             persistent: true
+        //         }
+        //     }
+        // ]),
+        RabbitMQModule.forRoot({
+            uri: process.env.RABBITMQ_URL || 'amqp://chaos_user:chaos_password@localhost:5672',
+            exchanges: [{name: 'chaos.exchange', type: 'topic'}],
+            connectionInitOptions: {
+                timeout: 1000
+            },
+        }),
     ],
 
     exports: [
-        ClientsModule,
+        RabbitMQModule,
         RabbitMQService
     ],
 
